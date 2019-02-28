c07ac8e99eefec09abbf5b34eb4d112464d22aaa
Stephen M. Coakley <me@stephencoakley.com>
2019-02-28 14:02:35 -0600

    Fix publishing CI step, attempt 2

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-GRADLE_PUBLISH_KEY=$(aws --region us-east-1 ssm get-parameter --name '/buildkite/gradle-publish-key' --with-decryption | jq -r '.Parameter.Value')
-GRADLE_PUBLISH_SECRET=$(aws --region us-east-1 ssm get-parameter --name '/buildkite/gradle-publish-secret' --with-decryption | jq -r '.Parameter.Value')
+GRADLE_PUBLISH_KEY=$(aws --region us-east-1 ssm get-parameter --with-decryption --name '/buildkite/gradle-publish-key' --output text --query Parameter.Value)
+GRADLE_PUBLISH_SECRET=$(aws --region us-east-1 ssm get-parameter --with-decryption --name '/buildkite/gradle-publish-secret' --output text --query Parameter.Value)
 
 echo "gradle.publish.key=$GRADLE_PUBLISH_KEY" >> ~/.gradle/gradle.properties
 echo "gradle.publish.secret=$GRADLE_PUBLISH_SECRET" >> ~/.gradle/gradle.properties
