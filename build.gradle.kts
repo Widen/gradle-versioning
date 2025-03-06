5507d9eca7533dd0bdcd9e8fb228379c1de1004e
Uriah Carpenter <uriah@widen.com>
2025-03-06 16:14:39 -0600

    Modernize Gradle Project
    - Use Gradle 8.13
    - Migrate to Kotlin build files

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+  `kotlin-dsl`
+  `java-gradle-plugin`
+  groovy
+  id("com.gradle.plugin-publish") version "1.3.1"
+  id("com.widen.versioning") version "0.4.0"
+}
+
+group = "com.widen.oss"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(11)
+  }
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  testImplementation("org.spockframework:spock-core:2.3-groovy-3.0")
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+    }
+}
+
+gradlePlugin {
+  plugins {
+    website.set("https://github.com/Widen/gradle-versioning")
+    vcsUrl.set("https://github.com/Widen/gradle-versioning")
+    plugins {
+      create("versioningPlugin") {
+        id = "com.widen.versioning"
+        implementationClass = "com.widen.versioning.VersioningPlugin"
+        displayName = "Widen Gradle versioning plugin"
+        description = "A Gradle plugin for applying project version from Git tags"
+        tags = listOf("versioning", "git",)
+      }
+    }
+  }
+}
--- src/test/groovy/com/widen/versioning/VersionGeneratorTest.groovy
+++ src/test/groovy/com/widen/versioning/VersionGeneratorTest.groovy
@@ -3,7 +3,10 @@ package com.widen.versioning
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.nio.file.Path
+
 class VersionGeneratorTest extends Specification {
+
     @Unroll("git describe '#describe' should become '#version'")
     def "parse git describe to version"() {
         expect:
@@ -16,4 +19,14 @@ class VersionGeneratorTest extends Specification {
         null   | "0.1.0-4-ge7426f0" | "0.1.0+4-ge7426f0"
         "v"    | "v1.2.3"           | "1.2.3"
     }
+
+    def "git command should execute"() {
+        when:
+        def dir = Path.of("").toAbsolutePath().toFile()
+        def git = VersionGenerator.generateFromGit(new Settings(), dir)
+
+        then:
+        git.isPresent()
+    }
+
 }
